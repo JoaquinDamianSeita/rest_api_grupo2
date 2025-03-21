57beae12266de80de0f5b25576583150e0729cfe
Joaquin Damian Seita <joaquindamianseita@gmail.com>
2025-03-20 23:27:09 -0300

    initial commit

---
+++ src/main/java/org/api/rest_api_grupo2/RestApiGrupo2Application.java
@@ -0,0 +1,13 @@
+package org.api.rest_api_grupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiGrupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiGrupo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/api/rest_api_grupo2/RestApiGrupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.api.rest_api_grupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiGrupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
