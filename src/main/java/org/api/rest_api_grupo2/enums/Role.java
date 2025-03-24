4f46483b1e79406e95ccf9d467c5dea39e105ed0
Joaquin Damian Seita <joaquindamian.seita@mercadolibre.com>
2025-03-24 20:07:50 -0300

    feat: add spring security

---
+++ src/main/java/org/api/rest_api_grupo2/enums/Role.java
@@ -0,0 +1,6 @@
+package org.api.rest_api_grupo2.enums;
+
+public enum Role {
+    BUYER,
+    ARTIST
+}
---
+++ src/main/java/org/api/rest_api_grupo2/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.api.rest_api_grupo2.repository;
+
+import org.api.rest_api_grupo2.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+}
