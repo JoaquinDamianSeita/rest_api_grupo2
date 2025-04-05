71c699466e8a6b8e04a942071726b4e4522dd163
Joaquin Seita <74945252+JoaquinDamianSeita@users.noreply.github.com>
2025-04-05 12:19:20 -0300

    Feature/add spring security (#5)
    * feat: add spring security
    
    * fix: add blank lines
    
    * fix: fix config add ping service
    
    ---------
    
    Co-authored-by: Joaquin Damian Seita <joaquindamian.seita@mercadolibre.com>

---
+++ src/main/java/org/api/rest_api_grupo2/model/User.java
@@ -0,0 +1,105 @@
+package org.api.rest_api_grupo2.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.api.rest_api_grupo2.enums.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "first_name")
+    private String firstName;
+
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "last_name")
+    private String lastName;
+
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    @Size(min = 1, max = 50)
+    private String username;
+
+    @NotNull
+    @Size(min = 8)
+    private String password;
+
+    @NotNull
+    @Size(min = 1, max = 50)
+    private String address;
+
+    @NotNull
+    private LocalDateTime registration_date;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/org/api/rest_api_grupo2/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package org.api.rest_api_grupo2.service.impl;
+
+import org.api.rest_api_grupo2.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+    }
+}
