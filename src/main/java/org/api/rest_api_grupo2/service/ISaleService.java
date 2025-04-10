452dd557f52db410d189ad1fd7036c941d694c1f
Ary Tomás <74367653+arytomasesc@users.noreply.github.com>
2025-04-10 18:08:21 -0300

    fix 2 (#21)

--- src/main/java/org/api/rest_api_grupo2/service/ISaleService.java
+++ src/main/java/org/api/rest_api_grupo2/service/ISaleService.java
@@ -2,7 +2,7 @@ package org.api.rest_api_grupo2.service;
 
 import java.util.List;
 
-import org.api.rest_api_grupo2.dto.request.SaleCreateRequest; // Ensure this class exists in the specified package
+import org.api.rest_api_grupo2.dto.request.SaleCreateRequest;
 import org.api.rest_api_grupo2.dto.response.SaleResponse;
 import org.api.rest_api_grupo2.model.Sale;
 
