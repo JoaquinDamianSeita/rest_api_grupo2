6aa40d33a226f7c1241d1ad1c9580ee3120434b6
Joaquin Seita <74945252+JoaquinDamianSeita@users.noreply.github.com>
2025-06-07 14:28:41 -0300

    fix username unique (#24)

--- src/main/java/org/api/rest_api_grupo2/repository/UserRepository.java
+++ src/main/java/org/api/rest_api_grupo2/repository/UserRepository.java
@@ -6,10 +6,12 @@ import org.api.rest_api_grupo2.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
+    List<Optional<User>> findByEmailOrUsername(String email, String username);
 }
